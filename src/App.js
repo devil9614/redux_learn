5392a983082630e7072b337d056b919a041ff827
sujan <sujan@nowpurchase.com>
2022-05-06 12:16:36 +0530

    test1 changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 import Login from './components/Login';
 import { Profile } from './components/Profile';
 import ToggleButton from './components/ToggleButton';
+
+const NewTest = () => {
+  return (
+    <button>Test1</button>
+  )
+}
+
 function App() {
   return (
     <div className="App">
+      <NewTest/>
       <ToggleButton/>
       <Profile/>
       <Login/>
