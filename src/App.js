1434db9a33b7491414d12c4cbdb159780570cf6f
sujan <sujan@nowpurchase.com>
2022-05-06 12:25:04 +0530

    commit in master branch after test1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Login from './components/Login';
 import { Profile } from './components/Profile';
 import ToggleButton from './components/ToggleButton';
+const mastercommit = "making a commit in the master branch after test 1 commits"
 function App() {
   return (
     <div className="App">
