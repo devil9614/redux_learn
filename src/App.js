7cfe19c721a60629e62819c8b3c04e0001da9c96
sujan <sujan@nowpurchase.com>
2022-05-10 12:55:31 +0530

    Using lazy and suspense for code splitting

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import { useSelector } from 'react-redux';
-import './App.css';
-import BlogPage from './components/BlogPage';
-import Login from './components/Login';
-import LoginButton from './components/LoginButton';
-import { Profile } from './components/Profile';
-import ToggleButton from './components/ToggleButton';
-const mastercommit = "making a commit in the master branch after test 1 commits"
+import { lazy, Suspense } from "react";
+import { useSelector } from "react-redux";
+import "./App.css";
+// import BlogPage from './components/BlogPage';
+// import LoginButton from './components/LoginButton';
+const BlogPage = lazy(() => import("./components/BlogPage"));
+const LoginButton = lazy(() => import("./components/LoginButton"));
+// const mastercommit = "making a commit in the master branch after test 1 commits"
 
-const NewTest = () => {
-  return (
-    <button>Test1</button>
-  )
-}
+// const NewTest = () => {
+//   return (
+//     <button>Test1</button>
+//   )
+// }
 
 function App() {
-  const isAuth = useSelector((state) => state.auth.value)
+  const isAuth = useSelector((state) => state.auth.value);
   return (
     <div className="App">
-      {/* <NewTest/>
-      <ToggleButton/>
-      <Profile/> */}
-      {/* <Login/> */}
-      <LoginButton/>
-      {isAuth && <BlogPage />}
+      <Suspense fallback = {<h3>Loading...</h3>}>
+        <LoginButton />
+        {isAuth && <BlogPage />}
+      </Suspense>
     </div>
   );
 }
